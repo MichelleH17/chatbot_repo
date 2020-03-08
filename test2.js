e73b126b4a2ca87a4522fd8459db860532c2fa5e
MichelleH17 <michelle.h@seznam.cz>
2020-03-08 14:27:42 +0100

    commit na call backy

--- test2.js
+++ test2.js
@@ -1,1 +1,8 @@
-verze z githubu
+function helloWorld() {
+    console.log('Hello World!');
+}
+
+const helloWorld = () => console.log('Hello World!');
+
+// callback funkce - zpětné volání
+// každá funkce je vlastně proměnná
